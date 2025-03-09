e2f87600a40428fa3cf7d5132f20aa58b953fb96
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-09 13:23:03 +0000

    feat:Added search and deletion to products component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -5,6 +5,35 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent  {
+export class ProductsComponent implements OnInit {
+  public products:any;
+  public keyword:string="";
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.products=[
+      {"id":1,"name":"computer","price":4333},
+      {"id":2,"name":"printer","price":3999},
+      {"id":3,"name":"keyboard","price":433},
+      {"id":4,"name":"mouse","price":333},
+    ];
+  }
+
+  deleteProduct(p: any) {
+    let index=this.products.indexOf(p);
+    this.products.splice(index,1);
+  }
+
+  search() {
+    console.log(this.keyword);
+    let result:any[] = [];
+    for(let p of this.products){
+      if(p.name.includes(this.keyword)){
+        result.push(p);
+      }
+    }
+    this.products = result;
+  }
 }
