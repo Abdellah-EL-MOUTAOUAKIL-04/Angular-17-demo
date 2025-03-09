56ce69169c80acdd26e57687a704780cf4c956a5
Abdellah EL MOUTAOUKIL <abdamota123@gmail.com>
2025-03-09 12:50:01 +0000

    feat:Creating products and home components

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent  {
+
+}
